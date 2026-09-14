e68c66cefe02bb886ed51e7476b8cff95b206bd7
Eligoon <derpepind117@live.dk>
2026-09-14 11:05:02 +0200

    main set up to test usercontroller and endpoint

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,14 +2,32 @@ package app;
 
 
 
+import app.config.HibernateConfig;
+import app.controllers.UserController;
+import app.daos.UserDAO;
 import app.dtos.UserDTO;
+import app.services.UserService;
 import io.javalin.Javalin;
+import jakarta.persistence.EntityManagerFactory;
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+
+        EntityManagerFactory emf =
+                HibernateConfig.getEntityManagerFactory();
+
+        UserDAO userDAO =
+                new UserDAO(emf);
+
+        UserService userService =
+                new UserService(userDAO);
+
+        UserController userController =
+                new UserController(userService);
+
         Javalin app =
                 Javalin.create(config -> {
 
@@ -20,17 +38,11 @@ public class Main {
                             )
                     );
 
-                    config.routes.get(
-                            "/test-json",
-                            ctx -> ctx.json(
-                                    new UserDTO(
-                                            1,
-                                            "Test User",
-                                            "test@test.com",
-                                            null
-                                    )
-                            )
+                    config.routes.post(
+                            "/api/users",
+                            userController::create
                     );
+
 
                 }).start(7070);
         }
